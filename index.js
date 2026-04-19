45855a7e35dda04d0abb852446b0a9bceecd9591
TuniDark <geeksgeeks2016@gmail.com>
2026-04-19 22:08:22 +0200

    fix token railway

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const generer = require('./commands/generer');
 const profil = require('./commands/profil');
